46c4579a179086417f5a46398c18bb5fd49ac0ec
stanlocktop <wlad-test@ukr.net>
2024-09-23 21:12:47 +0300

    if and else

--- Working with if-else, switch constructs/Program.cs
+++ Working with if-else, switch constructs/Program.cs
@@ -10,14 +10,60 @@ namespace Repeat
     {
         static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Hello, my name is Vladick:)");
-            Console.ResetColor();
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            //Console.WriteLine("Напиши число, щоб дізнатись парне врер чи ні");
+            //Console.ResetColor();
 
-            Console.WriteLine("Напишіть Ім'я");
-            string name = Console.ReadLine();
-            Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.WriteLine("Я люблю " + name);
+            //int a;
+            //a = int.Parse(Console.ReadLine());
+
+            //if (a % 2 == 0)
+            //    {
+            //    Console.WriteLine("Число парне");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Число не парне!");
+            //}
+            //Console.ReadKey();
+
+
+            Console.WriteLine("Введите номер дня недели (1 - понедельник, 7 - воскресенье): ");
+            if (int.TryParse(Console.ReadLine(), out int dayNumber))
+            {
+                switch (dayNumber)
+                {
+                    case 1:
+                        Console.WriteLine("1 - Понедельник");
+                        break;
+                    case 2:
+                        Console.WriteLine("2 - Вторник");
+                        break;
+                    case 3:
+                        Console.WriteLine("3 - Среда");
+                        break;
+                    case 4:
+                        Console.WriteLine("4 - Четверг");
+                        break;
+                    case 5:
+                        Console.WriteLine("5 - Пятница");
+                        break;
+                    case 6:
+                        Console.WriteLine("6 - Суббота");
+                        break;
+                    case 7:
+                        Console.WriteLine("7 - Воскресенье");
+                        break;
+                    default:
+                        Console.WriteLine("Ошибка, введите число от 1 до 7");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: введите корректное число.");
+            }
             Console.ReadKey();
         }
     }
